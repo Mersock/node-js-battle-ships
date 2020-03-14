acf3fc82d07fc7702a6a18307818fce343541775
knzphumthawan <knzphumthawan@gmail.com>
2020-03-14 22:34:14 +0700

    add route and validate.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { startGame, getGameStatus } from '../controller/game';
+import { placeShip } from '../controller/defender';
+import { validatePlaceShip } from '../validations/defender';
 
 const router = express.Router();
 
 router.get('/game/start', startGame);
 router.get('/game/:id/status', getGameStatus);
 
+router.post('/game/:id/place', validatePlaceShip, placeShip);
 export default router;
