88de5532b2034695c9b8d3505b6273ce0d254430
knzphumthawan <knzphumthawan@gmail.com>
2020-03-15 19:22:46 +0700

    imporve place model

--- src/controller/attack.js
+++ src/controller/attack.js
@@ -1,4 +1,6 @@
 import Game from '../models/game';
+import Place from '../models/place';
+import { getShipById } from '../utils/ship';
 import { responseWithError, responseWithData } from '../utils/response';
 
 export const attackShip = async (req, res) => {
@@ -20,6 +22,51 @@ export const attackShip = async (req, res) => {
       );
 
   const { xPosition, yPosition } = req.body;
+  const point = game.current_state[yPosition]?.[xPosition];
 
-  return res.send(game);
+  if (!point && point != 0)
+    return res.status(400).send(responseWithError('Invalid Positions.', 400));
+
+  game.attack_count++;
+
+  let status = '';
+  if (point == 0) status = 'Miss';
+  else {
+    // Checks remaining ship is left.
+    if (
+      game.current_state[yPosition - 1]?.[xPosition] === point ||
+      game.current_state[yPosition]?.[xPosition - 1] === point ||
+      game.current_state[yPosition + 1]?.[xPosition] === point ||
+      game.current_state[yPosition]?.[xPosition + 1] === point
+    )
+      status = 'Hit';
+
+    // ship has been sunk.
+    if (!status) {
+      const ship = getShipById(point);
+      status = 'You just sank a ' + ship;
+      game[ship]--;
+      game.total_ships--;
+    }
+
+    // when all ships have been sunk
+    if (game.total_ships == 0) {
+      status =
+        'Win! You have completed the game in ' + game.attack_count + ' moves';
+      game.status = 'ended';
+    }
+
+    game.current_state[yPosition][xPosition] = 0;
+    game.markModified('current_state');
+  }
+  await game.save();
+
+  const place = new Place();
+  place.game_id = game._id;
+  place.xPosition = xPosition;
+  place.yPosition = yPosition;
+  place.hit = point === 0;
+  await place.save();
+
+  return res.send(status);
 };
