39f90e4e95e0d857651daa49cea30bda1f4e3011
knzphumthawan <knzphumthawan@gmail.com>
2020-03-15 15:58:58 +0700

    init attack controller

--- src/controller/attack.js
+++ src/controller/attack.js
@@ -1,3 +1,11 @@
-export const attackShip = (req, res) => {
-  res.send(req.body);
+import Game from '../models/game';
+import { responseWithError } from '../utils/response';
+
+export const attackShip = async (req, res) => {
+  const _id = req.params.id;
+  const game = await Game.findById(_id);
+  if (!game) {
+    return res.status(404).send(responseWithError('Not Found.', 404));
+  }
+  return res.send(game);
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ import { startGame, getGameStatus } from '../controller/game';
 import { placeShip } from '../controller/place';
 import { validatePlaceShip } from '../validations/place';
 import { attackShip } from '../controller/attack';
+import { validateAttackShip } from '../validations/attack';
 
 const router = express.Router();
 
 router.get('/game/start', startGame);
 router.get('/game/:id/status', getGameStatus);
 router.post('/game/:id/place', validatePlaceShip, placeShip);
-router.post('/game/:id/attack', attackShip);
+router.post('/game/:id/attack', validateAttackShip, attackShip);
 
 export default router;
