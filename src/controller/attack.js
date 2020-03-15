a8b01c8c9167ebd76de72baf2b4610646ecd7a05
knzphumthawan <knzphumthawan@gmail.com>
2020-03-15 16:31:36 +0700

    handle status

--- src/controller/attack.js
+++ src/controller/attack.js
@@ -1,11 +1,25 @@
 import Game from '../models/game';
-import { responseWithError } from '../utils/response';
+import { responseWithError, responseWithData } from '../utils/response';
 
 export const attackShip = async (req, res) => {
   const _id = req.params.id;
   const game = await Game.findById(_id);
+  const { status } = game;
   if (!game) {
     return res.status(404).send(responseWithError('Not Found.', 404));
   }
+  if (status == 'ended')
+    return res
+      .status(400)
+      .send(responseWithError('This game already finish.', 400));
+  if (status != 'attacking')
+    return res
+      .status(400)
+      .send(
+        responseWithError('Cannot attack.Please place all you ships.', 400)
+      );
+
+  const { xPosition, yPosition } = req.body;
+
   return res.send(game);
 };
